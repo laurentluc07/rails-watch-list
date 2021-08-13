6f062e080f23bcab3791efeb0dd64cd90db4a68e
luc laurent <laurentluc7@gmail.com>
2021-08-13 08:58:36 +0200

    rails watch list

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,33 @@
 class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie = Movie.delete
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movie_path(@movie)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :poster_url, :rating)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :movies, :lists, :bookmarks
+  resources :lists, only: [:create, :new, :show,:index ] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
 end
